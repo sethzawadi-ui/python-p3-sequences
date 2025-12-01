d8b8656e56a751dfcfad395c09d12cdc0b32c1fe
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-12-01 10:31:02 +0300

    sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    # If length is 0, print empty list
+    if length == 0:
+        print([])
+        return
+
+    # Start Fibonacci list
+    fib = [0]
+
+    # If length is 1, print [0]
+    if length == 1:
+        print(fib)
+        return
+
+    # Add the second number
+    fib.append(1)
+
+    # Generate the remaining numbers
+    for i in range(2, length):
+        fib.append(fib[-1] + fib[-2])
+
+    # Print the entire list on one line
+    print(fib)
